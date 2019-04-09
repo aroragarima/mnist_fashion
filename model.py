82b78bb9702bedd99e88716bee82594131f9ecea
Garima Arora <garora2k9@gmail.com>
2019-04-09 11:51:13 +0530

    added print model summary function

--- model.py
+++ model.py
@@ -8,10 +8,10 @@ class CNN(object):
         self.opt = 1  # Adam
 
     def build_model(self):
-        model = Sequential()
+        self.model = Sequential()
 
         # Adding Convolutional layer with 32 outputs which indicates number of filters to be used are 32 with each of them of dimension 5 x 5
-        model.add(
+        self.model.add(
             Conv2D(
                 32,
                 kernel_size=(self.kernel_size, self.kernel_size),
@@ -21,32 +21,32 @@ class CNN(object):
             )
         )
         # Adding MAX Pooling Layer 2X2
-        model.add(MaxPooling2D((2, 2)))
+        self.model.add(MaxPooling2D((2, 2)))
         # Adding Padding = 1
-        model.add(ZeroPadding2D((1, 1)))
+        self.model.add(ZeroPadding2D((1, 1)))
         # Adding Convolutional layer with 32 inputs from CONV1, 64 outputs which indicates number of filters to be used are 64 with each of them of dimension 5 x 5
-        model.add(
+        self.model.add(
             Conv2D(
                 64, kernel_size=(self.kernel_size, self.kernel_size), activation="relu"
             )
         )
         # Adding MAX Pooling Layer 2X2
-        model.add(MaxPooling2D((2, 2)))
+        self.model.add(MaxPooling2D((2, 2)))
         # Adding Padding = 1
-        model.add(ZeroPadding2D((1, 1)))
+        self.model.add(ZeroPadding2D((1, 1)))
         # Adding 2D Convolution Layer with 128 Outputs Filter size 3x3
-        model.add(Conv2D(128, (3, 3), activation="relu"))
+        self.model.add(Conv2D(128, (3, 3), activation="relu"))
         # 	Adding Convolution Layer with 128 Outputs Filter size 5x5
-        model.add(Conv2D(128, (3, 3), activation="relu"))
+        self.model.add(Conv2D(128, (3, 3), activation="relu"))
         # Adding MAX Pooling Layer 2X2
-        model.add(MaxPooling2D((2, 2)))
-        model.add(Flatten())
+        self.model.add(MaxPooling2D((2, 2)))
+        self.model.add(Flatten())
         # Adding Fully Connected Layer1
-        model.add(Dense(256, activation="relu"))
+        self.model.add(Dense(256, activation="relu"))
         # Adding Fully Connected Layer2
-        model.add(Dense(256, activation="relu"))
+        self.model.add(Dense(256, activation="relu"))
         # SoftMax Layer for Classification
-        model.add(Dense(10, activation="softmax"))
+        self.model.add(Dense(10, activation="softmax"))
 
         if self.opt == 1:
             print("Adam Optimiser running")
@@ -56,15 +56,20 @@ class CNN(object):
             optimizer = keras.optimizers.RMSprop(lr=self.lr)
         else:
             print("Optimiser unrecognised, Please check help section for help")
-        model.compile(
+        self.model.compile(
             loss=keras.losses.categorical_crossentropy,
             optimizer=optimizer,
             metrics=["accuracy"],
         )
 
-        return model
+        return self.model
+
+    def print_summary(self):
+        return self.model.summary()
+
 
 
 if __name__ == "__main__":
     Custom_model = CNN(0.001, 5, "Adam")
-    print(Custom_model.build_model().summary())
+    Custom_model.build_model()
+    print(Custom_model.print_summary())
