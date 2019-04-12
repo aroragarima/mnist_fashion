c55fa28794da6d5991a7aca6b92380fe6ef678b9
Garima Arora <garora2k9@gmail.com>
2019-04-12 14:31:07 +0530

    Edited run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
- python3 train.py -lr 0.01 -s 128 -opt 1 --save_dir saved_models/ -aug 1 -test
+echo "Training with best model : \n \tLearning rate 0.01 \n \tOptimiser RMSProp"
+
+python3 train.py -lr 0.01 -s 128 -opt 2 --save_dir saved_models/ -aug 2 -train
